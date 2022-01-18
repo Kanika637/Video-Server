6baa6e520f1f5051bc3c6dba4deb33b22724e96d
kanika637 <kanikagola26@gmail.com>
2022-01-18 10:16:01 +0530

    Local database working fine

--- server/App.js
+++ server/App.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const mongoose=require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1/videoServer',{
-    useCreateIndex: true,
+    // useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
